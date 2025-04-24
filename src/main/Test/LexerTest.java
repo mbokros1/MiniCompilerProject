2de40900719b7aa5e45aad7c03a3bcadc5170900
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-04-23 20:07:23 -0700

    Added unit testing.

---
+++ src/main/Test/LexerTest.java
@@ -0,0 +1,44 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LexerTest {
+
+  @org.junit.jupiter.api.Test
+  void error() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void follow() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void char_lit() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void string_lit() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void div_or_comment() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void identifier_or_integer() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getToken() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getNextChar() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void printTokens() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void outputToFile() {
+  }
+}
